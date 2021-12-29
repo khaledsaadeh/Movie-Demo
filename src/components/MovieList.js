3834f11989e4e4b3a98669e9436c3a3dbd49291f
khaledsaadeh <60829323+khaledsaadeh@users.noreply.github.com>
2021-12-29 15:43:38 +0200

    Fetched and rendered the Movie list

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,9 +1,28 @@
-const MovieList = () =>{
 
-    return (<div>
-        Movie.
-    </div>)
-
-}       
+const MovieList = (props) => {
+  const setColor = color =>{
+    
+  }
+  return (
+    <div className="flex flex-wrap pl-20">
+      {props.movies.map((movie) => (
+        <div className="w-56 rounded overflow-hidden shadow-lg m-4">
+          <img
+            className="w-full"
+            src={`https://image.tmdb.org/t/p/w1280${movie.poster_path}`}
+            alt="poster"
+          ></img>
+          <div className="text-center">
+          <h2 className="font-bold text-xl mb-2 m-4">{movie.title}</h2>
+          <h3 className="m-4"> Release Date: {movie.release_date}</h3>
+          <span className="bg-red-400 font-bold rounded-xs p-1 ">
+            {movie.vote_average}
+          </span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default MovieList;
