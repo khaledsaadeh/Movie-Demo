03e1a80ac6ada116e4ecb5712752ab61823930e5
khaledsaadeh <60829323+khaledsaadeh@users.noreply.github.com>
2022-01-05 15:56:27 +0200

    Changed the search functionality according to comments, and used @apply for styling

--- src/App.js
+++ src/App.js
@@ -6,21 +6,36 @@ import { data } from "autoprefixer";
 import NavBar from "./components/NavBar";
 import Search from "./components/Search";
 
-const nowPlayingAPI =
+const NOWPLAYING_API =
   "https://api.themoviedb.org/3/movie/now_playing?api_key=4f298a53e552283bee957836a529baec&language=en-US&page=1";
+const SEARCH_API =
+  "https://api.themoviedb.org/3/search/movie?api_key=4f298a53e552283bee957836a529baec&language=en-US&page=1&include_adult=false&query=";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState("");
+
   useEffect(() => {
-    axios.get(nowPlayingAPI).then((res) => {
+    axios.get(NOWPLAYING_API).then((res) => {
       setMovies(res.data.results);
     });
   }, []);
 
+  const fetchSearch = (event) => {
+    event.preventDefault();
+    axios.get(SEARCH_API, { params: { query: search } }).then((res) => {
+      setMovies(res.data.results);
+    });
+  };
+
   return (
     <div className="App">
       <NavBar>
-        <Search setMovies={setMovies} movies={movies} />
+        <Search
+          setSearch={setSearch}
+          search={search}
+          fetchSearch={fetchSearch}
+        />
       </NavBar>
 
       <MovieList movies={movies} />
